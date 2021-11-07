988960af19857c415a34f72296d63770f4b453aa
SergioMG1259 <89159015+SergioMG1259@users.noreply.github.com>
2021-11-06 22:03:33 -0500

    feature: project

--- AppWeb Api/Startup.cs
+++ AppWeb Api/Startup.cs
@@ -1,8 +1,3 @@
-using AppWeb_Api.Domain.Repositories;
-using AppWeb_Api.Domain.Services;
-using AppWeb_Api.Persistence.Contexts;
-using AppWeb_Api.Persistence.Repositories;
-using AppWeb_Api.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,11 +8,30 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using SuperMarket_API.Persistence.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AppWeb_Api.BoundedPostulant.Domain.Repository;
+using AppWeb_Api.BoundedPostulant.Domain.Service;
+using AppWeb_Api.BoundedPostulant.Persistence.Repository;
+using AppWeb_Api.BoundedPostulant.Services;
+using AppWeb_Api.BoundedProject.Domain.Repository;
+using AppWeb_Api.BoundedProject.Domain.Service;
+using AppWeb_Api.BoundedProject.Persistence.Repository;
+using AppWeb_Api.BoundedProject.Services;
+using AppWeb_Api.BoundedCompany.Domain.Repository;
+using AppWeb_Api.BoundedCompany.Domain.Service;
+using AppWeb_Api.BoundedCompany.Persistence.Repository;
+using AppWeb_Api.BoundedCompany.Resource;
+using AppWeb_Api.BoundedCompany.Services;
+using AppWeb_Api.BoundedAnnouncement.Domain.Repository;
+using AppWeb_Api.BoundedAnnouncement.Domain.Service;
+using AppWeb_Api.BoundedAnnouncement.Persistence.Repository;
+using AppWeb_Api.BoundedAnnouncement.Services;
+using AppWeb_Api.Common.Domain.Repositories;
+using AppWeb_Api.Common.Persistence.Contexts;
+using AppWeb_Api.Common.Persistence.Repositories;
 
 namespace AppWeb_Api
 {
@@ -34,6 +48,7 @@ namespace AppWeb_Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo {Title = "AppWeb_Api", Version = "v1"}));
             services.AddDbContext<AppDbContext>(options =>
             {
                 options.UseInMemoryDatabase("appweb-api-in-memory");
@@ -42,6 +57,12 @@ namespace AppWeb_Api
             services.AddScoped<IPostulantService, PostulantService>();
             services.AddScoped<IProjectRepository, ProjectRepository>();
             services.AddScoped<IProjectService, ProjectService>();
+            services.AddScoped<IEvidenceRepository, EvidenceRepository>();
+            services.AddScoped<IEvidenceService, EvidenceService>();
+            services.AddScoped<ICompanyRepository, CompanyRepository>();
+            services.AddScoped<ICompanyService, CompanyService>();
+            services.AddScoped<IAnnouncementRepository, AnnouncementRepository>();
+            services.AddScoped<IAnnouncementService, AnnouncementService>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddAutoMapper(typeof(Startup));
         }
@@ -52,8 +73,8 @@ namespace AppWeb_Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                /*app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AppWeb_Api v1"));*/
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AppWeb_Api v1"));
             }
 
             app.UseHttpsRedirection();
